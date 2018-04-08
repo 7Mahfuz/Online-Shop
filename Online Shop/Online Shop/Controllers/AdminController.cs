1446600ed949f542d8c8b1bc86a6b5d4c6f73926
7Mahfuz <thehry01@gmail.com>
2018-04-08 08:53:01 +0600

    Dashboard and Categories list showing added

--- Online Shop/Online Shop/Controllers/AdminController.cs
+++ Online Shop/Online Shop/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Online_Shop.DAL;
+using Online_Shop.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,28 @@ namespace Online_Shop.Controllers
 {
     public class AdminController : Controller
     {
+        private GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+
+
+
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult Categories()
+        {
+            List<Category> AllCategories = _unitOfWork.GetRepositoryInstance<Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(AllCategories);
         }
     }
 }
